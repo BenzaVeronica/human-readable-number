629a59af0c89649a77922f4638e3271d96ade3ba
Veronica <benzaveronika@gmail.com>
2022-06-22 18:02:42 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    const numbers = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const tens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const types = {
+        2: "hundred",
+        3: "thousand",
+        6: "million",
+    };
+    let arr = String(num).split("");
+    let newArr = [];
+    switch (arr.length) {
+        case 1:
+            return numbers[arr[0]];
+        case 2:
+            return tens[arr[1]];
+        default:
+            for (let i = arr.length - 1; i >= 0; i--) {
+                if (i === 1)
+                    newArr.push(
+                        dozens[arr.shift()] + " " + numbers[arr.shift()]
+                    );
+                if (types[i])
+                    newArr.push(numbers[arr.shift()] + " " + types[i]);
+            }
+    }
+    return newArr.join(" ");
+};
